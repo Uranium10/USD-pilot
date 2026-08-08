c94d15b4c14d6837450d6efe0d91626d2ea3d4d8
Uranium10 <kdm10ho@naver.com>
2026-08-09 05:44:13 +0900

    Restore seven-day cycle length

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 const viteEnv = import.meta.env || {}
 
 export const DAY_DURATION_SECONDS = Number(viteEnv.VITE_DAY_DURATION_SECONDS) || 8 * 60
-export const DAYS_PER_CYCLE = 3
+export const DAYS_PER_CYCLE = 7
 export const MAX_CYCLES = 6
 export const TICK_MS = 100
 export const MAX_ENERGY = 100
