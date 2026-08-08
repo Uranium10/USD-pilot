86680b58ed2d894e7a630385800efa2c4c8e612e
Uranium10 <kdm10ho@naver.com>
2026-08-09 06:34:32 +0900

    Update: add night shop thumbnails and styling

--- src/data/nightContent.js
+++ src/data/nightContent.js
@@ -2,6 +2,7 @@ export const NIGHT_ITEMS = {
   chiliEnergy: {
     id: 'chili-energy',
     name: '칠리맛 에너지 드링크',
+    img: '/imgs/items/chili_energy.png',
     description: '매콤함과 카페인을 같은 캔에 가둔 수상한 음료.',
     price: 150,
     energyRestore: 8,
@@ -12,6 +13,7 @@ export const NIGHT_ACTIVITIES = {
   convenienceJob: {
     id: 'convenience-job',
     name: '편의점 아르바이트',
+    img: '/imgs/items/convenience_job.png',
     description: '궤도 정거장 편의점의 야간 재고를 정리한다.',
   },
 }
